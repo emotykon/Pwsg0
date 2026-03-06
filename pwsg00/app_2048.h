21b178ab29030b533bcb6dbf7ad3d9351545e088
emotykon <132263269+emotykon@users.noreply.github.com>
2026-03-06 11:55:51 +0100

    Add files via upload

---
+++ pwsg00/app_2048.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <windows.h>
+#include <string>
+class app_2048
+{
+private:
+	bool register_class(); ///zapisuje klase
+	static std::wstring const s_class_name;//nazwa okna
+	static LRESULT CALLBACK window_proc_static(
+		HWND window, UINT message,
+		WPARAM wparam, LPARAM lparam);//statyczna obs³uga zdarzeñ - posrednikC-C++
+	LRESULT window_proc(
+		HWND window, UINT message,
+		WPARAM wparam, LPARAM lparam);//w³asciwa obsluga komunikatow
+	HWND create_window();//tworzy okno
+	HINSTANCE m_instance;//uchwyt do obecnej instancji aplikacji
+	HWND m_main;//uchwyt do okna
+public:
+	app_2048(HINSTANCE instance);//konstruktor klasy
+	int run(int show_command);//petlakomunikatow
+};
---
+++ pwsg00/main.cpp
@@ -0,0 +1,86 @@
+﻿#define WIN32_LEAN_AND_MEAN //wyklucza rzadko używane czesci biblioteki
+#define NOMINMAX //konflikt z c++ wyklucza
+#include <windows.h> //bib winapi
+#include "app_2048.h" // biblioteka nasza
+int WINAPI wWinMain(HINSTANCE instance/*naszainstancja*/,
+	HINSTANCE /*starezawszenull*/,
+	LPWSTR /*argumentyprogramu*/,
+	int show_command/*sposóbwyswietlania*/)
+{
+	app_2048 app{ instance };//tworzy wszystko
+	return app.run(show_command);//przetwarza wiadomosci
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#define WIN32_LEAN_AND_MEAN
+//#define NOMINMAX
+//#include <windows.h>
+//
+//int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+//{
+//    // 1. Poprawiona struktura (przecinki zamiast średników, dodany hInstance)
+//    LPCWSTR clsname = L"My Window Class";
+//    WNDCLASSEXW wcx = { 0 };
+//    wcx.cbSize = sizeof(WNDCLASSEXW);
+//    wcx.lpfnWndProc = DefWindowProcW;
+//    wcx.hInstance = hInstance; // To jest wymagane!
+//    wcx.lpszClassName = clsname;
+//    wcx.hCursor = LoadCursor(NULL, IDC_ARROW); // Dodaj kursor, żeby nie znikał
+//
+//    ATOM clsid = RegisterClassExW(&wcx);
+//    if (!clsid) return -1;
+//
+//    // 2. Zmieniony styl z 0 na WS_OVERLAPPEDWINDOW, żeby okno było widać
+//    HWND hWnd = CreateWindowExW(0, clsname, L"Hello World!",
+//        WS_OVERLAPPEDWINDOW, 100, 100, 640, 360, nullptr,
+//        nullptr, hInstance, nullptr); // hInstance zamiast nullptr
+//    HWND hWnd2 = CreateWindowExW(0, clsname, L"Hello World!",
+//        WS_OVERLAPPEDWINDOW, 100, 500, 640, 360, nullptr,
+//        nullptr, hInstance, nullptr);
+//
+//    if (!hWnd) return -1;
+//
+//    ShowWindow(hWnd, nShowCmd);
+//    ShowWindow(hWnd2, nShowCmd);
+//
+//    MSG msg = { 0 };
+//    while (GetMessageW(&msg, nullptr, 0, 0) > 0) {
+//        TranslateMessage(&msg); // Dobre praktyki
+//        DispatchMessageW(&msg);
+//    }
+//
+//    return 0;
+//}
